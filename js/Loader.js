c817899e0c79debb15a30f56250a10b17cd02179
simonBtssio <simon.farnault.sio@gmail.com>
2023-03-01 11:07:58 +0100

    C4

--- js/Loader.js
+++ js/Loader.js
@@ -5,6 +5,13 @@ class Loader{
     dateBar = document.getElementById("date-bar");
     showBat = true;
 
+    openClose = document.getElementById("open-close");
+    viewClose = document.getElementById("close_app");
+    closeOpen = false;
+
+    c_lock = document.getElementById("c_lock");
+    c_close = document.getElementById("c_close");
+
     constructor(){
 
         this.dateBar.innerHTML = this.dateTime.getDate();
@@ -15,6 +22,24 @@ class Loader{
             pourcentage.style.height = calcul;
         });
         this.setListenerBat();
+
+        this.openClose.addEventListener("click", () => {
+            if(this.closeOpen){
+                this.viewClose.style.display = "none";
+                this.closeOpen = false;
+            }else{
+                this.viewClose.style.display = "flex";
+                this.closeOpen = true;
+            }
+        });
+
+        this.c_lock.addEventListener("click", () => {
+            document.getElementById("login-show").style.display = "flex";
+        });
+
+        this.c_close.addEventListener("click", () => {
+            window.close();
+        });
     }
 
     setListenerBat(){
